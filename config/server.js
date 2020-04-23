69c31877cc883c4965533f08b50b0f59e6004b31
123PedroAugusto1 <45266843+123PedroAugusto1@users.noreply.github.com>
2020-04-23 17:56:53 -0300

    The application was structured

---
+++ config/server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var consign = require('consign');
+var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
+
+
+
+var app = express();
+app.set('view engine','ejs');
+
+
+
+
